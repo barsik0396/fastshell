d4ced4749f8249df13fb39ee65e20946ee097cb6
Barsik <barsik0396@outlook.com>
2026-08-24 11:15:30 +0300

    update version in help; add new commands to help

--- commands/help.c
+++ commands/help.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 void help(void) {
-    printf("fastshell v0.1.0 - commands:\n");
+    printf("fastshell v0.2.0 - commands:\n");
     printf("  help               show this help\n");
     printf("  cycle              just \"for (i <= 1000000)\"...\n");
     printf("  sh                 run non-fastshell commands\n");
+    printf("  reload             restart fastshell");
+    printf("  whoami             who am i?");
 }
