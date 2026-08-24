dc4b17d0b7033a028fd617758945a2eaddeae1f1
Barsik <barsik0396@outlook.com>
2026-08-24 10:13:41 +0300

    print user in prompt

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include "call.h"
 #include "input.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 #ifdef _WIN32
 #error "do not compile fastshell on windows!"
 #endif
 
 int main(void) {
+    char *user = getenv("USER");
+    if (!user) {
+        printf("\x1b[1;37mfastshell\x1b[0m \x1b[33;1mwarn\x1b[0m no USER var found in env!\n");
+    }
     while (1) {
         char buffer[8192];
-        printf("fastshell# ");
+        if (user) {
+            printf("(%s) fastshell# ", user);
+        } else {
+            printf("fastshell# ");
+        }
         char *output = input(buffer);
         call(output);
     }
